a48500aef3e1dcdcfdd8f2b062e7af926a23bc21
tomcorey26 <tomcorey26@gmail.com>
2019-01-28 18:37:27 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <HeaderNav/>
     );
   }
 }
