c50bf1b95531f1a0da236de6692580f9f25d7367
tomcorey26 <tomcorey26@gmail.com>
2019-02-14 20:49:41 -0500

    Added subscriptions and footer to sidebar

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {SideBarItem} from './SideBarItem/SideBarItem';
-import {Menu} from 'semantic-ui-react';
+import {Menu, Divider} from 'semantic-ui-react';
 import './SideBar.scss';
+import {Subscriptions} from './Subscriptions/Subscriptions';
+import {SideBarFooter} from './SideBarFooter/SideBarFooter';
+
 
 export class SideBar extends React.Component {
   render() {
@@ -10,13 +13,21 @@ export class SideBar extends React.Component {
         <SideBarItem highlight={true} label='Home' icons='home'/>
         <SideBarItem label='Trending' icons='fire'/>
         <SideBarItem label='Followers' icons='spy'/>
+        <Divider/>
         <SideBarItem label='History' icons='history'/>
         <SideBarItem label='Watch later' icons='clock'/>
         <SideBarItem label='Liked videos' icons='thumbs up'/>
+        <Divider/>
+        <Subscriptions/>
         <SideBarItem label='Movies and Shows' icons='film'/>
+        <Divider/>
         <SideBarItem label='Report history' icons='flag'/>
         <SideBarItem label='Help' icons='help circle'/>
         <SideBarItem label='Send feedback' icons='comment'/>
+        <Divider/>
+        <SideBarItem label='Send feedback' icon='comment'/>
+        <Divider/>
+        <SideBarFooter/>
       </Menu>
     )
   }
