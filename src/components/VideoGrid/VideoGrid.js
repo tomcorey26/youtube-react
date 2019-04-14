65c33fb3b6b98ff0f5f685d3dfe581efa7635bda
tomcorey26 <tomcorey26@gmail.com>
2019-04-14 19:06:20 -0400

    Created video grid component

---
+++ src/components/VideoGrid/VideoGrid.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./VideoGrid.scss";
+import { Divider } from "semantic-ui-react";
+import { VideoPreview } from "../VideoPreview/VideoPreview";
+
+export function VideoGrid(props) {
+  const divider = props.hideDivider ? null : <Divider />;
+  return (
+    <React.Fragment>
+      <h4>Trending</h4>
+      <div className="video-grid">
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+        <VideoPreview />
+      </div>
+      {divider}
+    </React.Fragment>
+  );
+}
