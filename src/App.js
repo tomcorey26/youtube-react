86eaabceb3958a5fde539463094dfbf63e90d5cc
tomcorey26 <tomcorey26@gmail.com>
2019-04-14 17:15:07 -0400

    added home and video preview

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import HeaderNav from './containers/HeaderNav/HeaderNav';
-import {SideBar} from './containers/SideBar/SideBar'
-
+import React, { Component } from "react";
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
+import { SideBar } from "./containers/SideBar/SideBar";
+import { Home } from "./containers/Home/Home";
 
 class App extends Component {
   render() {
     return (
-    <React.Fragment>
-      <HeaderNav/>
-      <SideBar/>
-    </React.Fragment>
+      <React.Fragment>
+        <HeaderNav />
+        <SideBar />
+        <Home />
+      </React.Fragment>
     );
   }
 }
